edd2dcbfb7062dca6703b64796b2ff540b845072
jejacks0n <jejacks0n@gmail.com>
2019-06-11 16:53:13 -0600

    Testing a ci fix.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,7 +2,7 @@ def register_driver(name, args = [], opts = {})
   Capybara.register_driver(name) do |app|
     options = { args: args + ["window-size=1440,1080"] }
     options[:binary] = ENV.fetch("GOOGLE_CHROME_SHIM", nil)
-    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: options.compact)
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome("goog:chromeOptions" => options.compact)
     Capybara::Selenium::Driver.new(app, { browser: :chrome, desired_capabilities: capabilities }.merge(opts))
   end
 end
